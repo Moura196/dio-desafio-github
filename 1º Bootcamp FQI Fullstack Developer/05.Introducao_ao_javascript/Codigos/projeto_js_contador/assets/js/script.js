8fc3fd8f4068769350eb0b40212f1c46fb65a8a5
Gabriel Mendes de Moura <gmendesmoura@gmail.com>
2024-08-16 19:09:18 -0300

    Atualização de conteúdo: Após seguir instruções da aula de introdução de JavaScript, realizei as atividades propostas.

--- 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/projeto_js_contador/assets/js/script.js
+++ 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/projeto_js_contador/assets/js/script.js
@@ -1,14 +1,55 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
+// following instructions from DIO Class about JavaScript introduction:
+
+// function increment(){
+//     currentNumber = currentNumber + 1;
+//     currentNumberWrapper.innerHTML = currentNumber; // innerHTML pega o elemento HTML
+// }
+
+// function decrement(){
+//     currentNumber = currentNumber - 1;
+//     currentNumberWrapper.innerHTML = currentNumber; // innerHTML pega o elemento do HTML
+// }
+
+// see activities at https://github.com/stebsnusch/basecamp-javascript/tree/main/introducao-ao-javascript/contador
+// Activities: 1: OK; 2: OK; 3: OK;
+
+document.getElementById('subtrair').addEventListener('click', decrement);
+        
+document.getElementById('adicionar').addEventListener('click', increment);
+
+document.getElementById('subtrair').addEventListener('click', changeColors);
+
+document.getElementById('adicionar').addEventListener('click', changeColors);
+
 function increment(){
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber; // innerHTML pega o HTML
+    // criando condições para permitir ou não que a função seja ativada
+    if (currentNumber < 10) {
+        currentNumber = currentNumber + 1;
+        document.getElementById('currentNumber').innerHTML = currentNumber;
+    } else {
+        alert('Você não pode aumentar mais este número!')
+    }
 }
 
 function decrement(){
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber; // innerHTML pega o elemento do HTML
+    // criando condições para permitir ou não que a função seja ativada
+    if (currentNumber > -10) {
+        currentNumber = currentNumber - 1;
+        document.getElementById('currentNumber').innerHTML = currentNumber;
+    } else {
+        alert('Você não pode diminuir mais este número!');
+    }
 }
 
-// see activities at https://github.com/stebsnusch/basecamp-javascript/tree/main/introducao-ao-javascript/contador
+function changeColors() {
+    if (currentNumber < 0) {
+        document.getElementById('currentNumber').style.color = 'red';
+    } else if (currentNumber > 0) {
+        document.getElementById('currentNumber').style.color = 'blue';
+    } else {
+        document.getElementById('currentNumber').style.color = 'black';
+    }
+}
