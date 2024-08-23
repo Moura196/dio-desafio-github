829e259d99d1d4aaaa78581cd0b8e589ff5b0766
Gabriel Mendes de Moura <gmendesmoura@gmail.com>
2024-08-23 10:59:58 -0300

    Atualização de conteúdo: Tipos de funções.

---
+++ 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/calculadora/funcoes.js
@@ -0,0 +1,25 @@
+// TIPOS DE FUNÇÃO
+
+// DECLARATIVAS
+
+function funcao() {
+    console.log('Sou uma função declarativa')
+}
+
+funcao();
+
+// ESPRESSAO DE FUNÇÃO
+
+// COM NOMEAÇÃO
+
+var funcao = function() {
+    console.log('Sou uma função de expressão')
+}
+
+// ARROW FUNCTION
+
+var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+
+funcao();
