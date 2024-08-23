e7c1bd5316b05371f4b9157aea6276cec7973ed0
Gabriel Mendes de Moura <gmendesmoura@gmail.com>
2024-08-23 10:59:26 -0300

    Atualização de conteúdo: Estruturas de repetições e Estruturas condicionais.

--- 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/introducao.js
+++ 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/introducao.js
@@ -101,83 +101,93 @@
 
 // ESTRUTURAS CONDICIONAIS
 
-var jogador1 = 0;
-var jogador2 = 0;
-var placar;
+// var jogador1 = 0;
+// var jogador2 = 0;
+// var placar;
 
-if (jogador != -1) { // Ninho ou aninhamento de if
-    if (jogador1 > 0 && jogador2 == 0) {
-        console.log('Jogador 1 pontuou!');
-        placar = jogador1 > jogador2;
-} else if (jogador2 > 0) {
-        console.log('Jogador 2 pontuou!');
-        placar = jogador2 > jogador1;
-    } else {
-        console.log('Ninguem marcou ponto')
-    }
+// if (jogador != -1) { // Ninho ou aninhamento de if
+//     if (jogador1 > 0 && jogador2 == 0) {
+//         console.log('Jogador 1 pontuou!');
+//         placar = jogador1 > jogador2;
+// } else if (jogador2 > 0) {
+//         console.log('Jogador 2 pontuou!');
+//         placar = jogador2 > jogador1;
+//     } else {
+//         console.log('Ninguem marcou ponto')
+//     }
+// }
+
+// // if ternário
+// jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos!') : console.log('Jogadores são inválidos!')
+
+// // SWITCH/CASE
+// switch (placar) {
+//     case placar = jogador1 > jogador2:
+//         console.log('Jogador 1 ganhou!');
+//         break;
+//     case placar = jogador2 > jogador1:
+//         console.log('Jogador 2 ganhou!');
+//         break;
+//     default:
+//         console.log('Ninguem ganhou!');
+// }
+
+// // ESTRUTURAS DE REPETIÇÃO
+// // for
+//     //estrutura for([expressaoInicial];[condicao];[incremento]){declaracao}
+// let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
+
+// let object = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3'};
+
+// for (let indice =0; indice < array.length; indice++) {
+//     console.log(indice);
+// }
+
+// //for in 
+//     //estrutura for([indice] in [objeto ou array]) {declaracao}
+//     //com array
+// for (let i in array) {
+//     console.log(i);
+// }
+
+//     //com objeto
+// for (i in object) { //javascript já entende que é variável de estado
+//     console.log(i);
+// }
+
+// //for of
+//     //estrutura for([indice] of [array]){declaracao}
+//     //com array
+// for (i of array) {
+//     console.log(i);
+// }
+
+//     //com object
+//     //objects não são interáveis(elas variam), já o array são números inteiros
+// for (i of object.propriedade1) {
+//     console.log(i);
+// }
+
+// //while
+//     //estrutura while(condicao){declaracao}
+// var a = 0;
+// while (a < 10) {
+//     a++;
+//     console.log(a);
+// }
+
+// //do while
+// do {
+//     a++;
+//     console.log(a)
+// } while (a < 10)
+
+// FUNÇÕES COM PARÂMETROS
+
+//Servem como variáveis de estado(parâmetros)
+
+function menssagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
 }
 
-// if ternário
-jogador1 != -1 && jogador2 != -1 ? console.log('Os jogadores são válidos!') : console.log('Jogadores são inválidos!')
-
-// SWITCH/CASE
-switch (placar) {
-    case placar = jogador1 > jogador2:
-        console.log('Jogador 1 ganhou!');
-        break;
-    case placar = jogador2 > jogador1:
-        console.log('Jogador 2 ganhou!');
-        break;
-    default:
-        console.log('Ninguem ganhou!');
-}
-
-// ESTRUTURAS DE REPETIÇÃO
-// for
-    //estrutura for([expressaoInicial];[condicao];[incremento]){declaracao}
-let array = ['valor1', 'valor2', 'valor3', 'valor4', 'valor5'];
-
-let object = {propriedade1: 'valor1', propriedade2: 'valor2', propriedade3: 'valor3'};
-
-for (let indice =0; indice < array.length; indice++) {
-    console.log(indice);
-}
-
-//for in 
-    //estrutura for([indice] in [objeto ou array]) {declaracao}
-    //com array
-for (let i in array) {
-    console.log(i);
-}
-
-    //com objeto
-for (i in object) { //javascript já entende que é variável de estado
-    console.log(i);
-}
-
-//for of
-    //estrutura for([indice] of [array]){declaracao}
-    //com array
-for (i of array) {
-    console.log(i);
-}
-
-    //com object
-    //objects não são interáveis(elas variam), já o array são números inteiros
-for (i of object.propriedade1) {
-    console.log(i);
-}
-
-//while
-    //estrutura while(condicao){declaracao}
-var a = 0;
-while (a < 10) {
-    a++;
-    console.log(a);
-}
-
-//do while
-do {
-    a++;
-    console.log(a)
-} while (a < 10)
+menssagem('Tudo certo', 'jovem!');
