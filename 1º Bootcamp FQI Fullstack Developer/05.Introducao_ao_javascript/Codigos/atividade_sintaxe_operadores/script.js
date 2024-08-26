fd853169b5bf0c4066d868bdfb8e5abe203abaeb
Moura196 <gmendesmoura@gmail.com>
2024-08-26 18:27:09 -0300

    :construction: Atualização da atividade, com outra forma de fazer.

--- 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/atividade_sintaxe_operadores/script.js
+++ 1º Bootcamp FQI Fullstack Developer/05.Introducao_ao_javascript/Codigos/atividade_sintaxe_operadores/script.js
@@ -1,25 +1,38 @@
-var a = prompt('Digite um número:');
-var b = prompt('Digite outro número:');
+// function atividade(a, b) {
+//     let soma = a + b;
 
-function atividade(a, b) {
+//     if (a == b) {
+//         if (soma > 10 && soma > 20) {
+//             alert(`Os números ${a} e ${b} são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20!`);
+//         } else if (soma > 10 && soma < 20) {
+//             alert(`Os números ${a} e ${b} são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20!`);
+//         } else {
+//             alert(`Os números ${a} e ${b} são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20!`);
+//         }
+//     } else {
+//         if (soma > 10 && soma > 20) {
+//             alert(`Os números ${a} e ${b} não são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20!`);
+//         } else if (soma > 10 && soma < 20) {
+//             alert(`Os números ${a} e ${b} não são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20!`);
+//         } else {
+//             alert(`Os números ${a} e ${b} não são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20!`);
+//         }
+//     }
+// }
+
+// atividade(7, 2);
+
+function comparacao(a, b) {
+    const saoIguais = a == b;
+
+    (saoIguais) ? alert('são iguais') : alert('não são iguais');
+    soma(a, b);
+}
+
+function soma(a, b) {
     let soma = a + b;
 
-    if (a == b) {
-        console.log('iguais');
-        if (soma > 10 && soma > 20) {
-            alert(`Os números ${a} e ${b} são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20!`);
-        } else if (soma > 10 && soma < 20) {
-            alert(`Os números ${a} e ${b} são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20!`);
-        } else {
-            alert(`Os números ${a} e ${b} são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20!`);
-        }
-    } else {
-        if (soma > 10 && soma > 20) {
-            alert(`Os números ${a} e ${b} não são iguais. Sua soma é ${soma}, que é maior que 10 e maior que 20!`);
-        } else if (soma > 10 && soma < 20) {
-            alert(`Os números ${a} e ${b} não são iguais. Sua soma é ${soma}, que é maior que 10 e menor que 20!`);
-        } else {
-            alert(`Os números ${a} e ${b} não são iguais. Sua soma é ${soma}, que é menor que 10 e menor que 20!`);
-        }
-    }
+    (soma > 10) ? alert('maior que 10') : alert('menor que 10');
 }
+
+comparacao(1, 2);
